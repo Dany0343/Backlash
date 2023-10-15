2d15472bede84b7116bb0f6930d213efe5fb29d0
Oscar Bucio <oscarbucio2001@gmail.com>
2023-10-14 21:01:54 -0600

    added: nextui

---
+++ src/app/upload/page.jsx
@@ -0,0 +1,14 @@
+import Header from '../Header';
+
+export default function Upload() {
+    return (
+        <>
+        <Header/>
+      
+        <div className='h-[800px] border-2 border-white m-9 rounded-md'>
+            
+        </div>
+        </>
+      )
+  }
+  
