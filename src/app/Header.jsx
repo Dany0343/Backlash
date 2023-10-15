b41c153bc19816bf4c3ec33d5ae9fc16f5a6aac6
Oscar Bucio <oscarbucio2001@gmail.com>
2023-10-15 12:28:12 -0600

    loading screen loaded

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -1,12 +1,13 @@
+"use client"
 import Link from 'next/link'
 
 export default function Header() {
   return (
     <header>
       <ul className="w-full flex flex-row justify-center items-center gap-20">
-        <Link href="./upload">Upload</Link>
-        <Link href="/" className='text-5xl font-bold glitch' data-glitch='Backlash'>Backlash</Link>
-        <Link href="./community">Community</Link>
+        <Link href="./upload" className='cursor-pointer text-white font-bold text-xl'>Upload</Link>
+        <Link href="/" className='text-5xl font-bold glitch cursor-pointer' data-glitch='Backlash'>Backlash</Link>
+        <Link href="./community" className='cursor-pointer text-white font-bold text-xl'>Community</Link>
       </ul>
     </header>
   );
---
+++ src/app/loadingScreen.jsx
@@ -0,0 +1,65 @@
+"use client"
+
+import { useSpring, animated } from "react-spring";
+import { useState } from "react";
+import Image from "next/image";
+import Logo from "../../public/images/backlashInner.svg";
+
+export const LoadingScreen = ({ onFinished }) => {
+  const [phase, setPhase] = useState(0);
+
+  // Animación de pulso
+  const pulseProps = useSpring({
+    to:
+      phase === 0
+        ? [
+            { transform: "scale(1.1)" },
+            { transform: "scale(1)" },
+            { transform: "scale(1.1)" },
+            { transform: "scale(1)" },
+            { transform: "scale(1.1)" },
+            { transform: "scale(1)" },
+          ]
+        : { transform: "scale(1)" },
+    config: { tension: 300, friction: 15 },
+    onRest: () => setPhase(phase + 1),
+  });
+
+  // Animación de encogimiento
+  const shrinkProps = useSpring({
+    to:
+      phase === 1
+        ? { opacity: 0, transform: "scale(0)", gradient: 100 }
+        : { opacity: 1, transform: "scale(1)", gradient: 25 },
+    delay: 500,
+    config: { duration: 1500 },
+    onRest: onFinished,
+    reset: phase === 1,
+  });
+
+  // Wrapear componente de NextJS Image para que podamos animarlo
+  const AnimatedImg = animated(Image);
+
+  return (
+    <animated.div
+      className="fixed top-0 left-0 w-full h-full flex items-center justify-center"
+      style={{
+        background: shrinkProps.gradient.to(
+          (gradient) =>
+            `radial-gradient(circle, rgba(0,0,0,1) ${gradient}%, rgba(31,41,55,1) 100%)`
+        ),
+      }}
+    >
+      <AnimatedImg
+        src={Logo}
+        alt={"Enterprise Logo"}
+        style={phase === 0 ? pulseProps : shrinkProps}
+        className={
+          "h-[455px] xl:w-[455px] xl:h-[455px] lg:w-[455px] lg:h-[100px] md:w-[365px] md:h-[365px] sm:w-[200px] sm:h-[200px]"
+        }
+      />
+    </animated.div>
+  );
+};
+
+export default LoadingScreen;
