68481f1e734c331db9f41fe98798dbc877cdc4fe
Oscar Bucio <oscarbucio2001@gmail.com>
2023-11-07 10:50:53 -0600

    kinda done

--- src/app/watch/page.jsx
+++ src/app/watch/page.jsx
@@ -22,7 +22,7 @@ import TalkToMe from "../../../public/images/TalktoMe.png";
 import TheCreator from "../../../public/images/thecreator.png";
 import TheExorcist from "../../../public/images/theexorcist.png";
 import TheShining from "../../../public/images/theshining.png";
-import Cinema from "../../../public/images/cinema.png"
+import Cinema from "../../../public/images/cinema.png";
 
 export default function Watch() {
   // Se usa el hook
@@ -37,7 +37,7 @@ export default function Watch() {
   return (
     <>
       <Header />
-      <div className="flex md:flex-col sm:flex-col sm:gap-4 m-9">
+      <div className="flex md:flex-col md:justify-center lg: justify-center sm:flex-col sm:gap-4 m-9">
         <p className=" text-white font-bold lg:text-4xl md:text-3xl md:self-center lg:ml-20 sm:text-xl">
           {title}
         </p>
@@ -48,18 +48,26 @@ export default function Watch() {
           alt="Picture of the author"
           className="rounded-md lg:w-[400px] lg:h-[400px] md:w-[300px] md:h-[300px] sm:w-[250px] sm:h-[250px] sm:self-center"
         ></Image> */}
-        <p className=" text-white font-bold lg:text-4xl md:text-xl md:self-center lg:ml-20 sm:text-xl">
+        <div className="h-[800px] border-2 border-white m-9 rounded-md">
+          <iframe
+            id="player"
+            width="100%"
+            height="100%"
+            src="https://backlash.blob.core.windows.net/dev/prueba"
+            title="Backash streaming"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+        <p className=" text-white font-bold lg:text-2xl md:text-xl md:self-center sm:text-xl">
           {description}
         </p>
-        <p className=" text-white font-bold lg:text-4xl md:text-xl md:self-center lg:ml-20 sm:text-xl">
-          {user}
+        
+        <p className=" text-white font-bold lg:text-4xl md:text-xl md:self-center sm:text-xl">
+          Autor: {user}
         </p>
       </div>
       <div className="flex justify-center mt-5 gap-12">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded md:text-3xl lg:text-5xl">
-          <Link href="./community">Ver pelicula</Link>
-        </button>
-
         <button className="bg-transparent hover:bg-blue-500 text-white font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded md:text-3xl lg:text-5xl">
           <Link href="/">Volver</Link>
         </button>
