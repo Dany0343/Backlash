04eb9726e046c7d5f1cc2f3775e0c21062b2c594
Oscar Bucio <oscarbucio2001@gmail.com>
2023-11-09 12:13:37 -0600

    api routes kinda

--- src/app/upload/page.jsx
+++ src/app/upload/page.jsx
@@ -21,8 +21,8 @@ export default function Upload() {
     };
 
     // Fetch a API Route
-    const response = await fetch("/api/submitForm", {
-      method: "POST",
+    const response = await fetch("/api/movies", {
+      method: 'POST',
       headers: {
         "Content-Type": "application/json",
       },
@@ -31,9 +31,9 @@ export default function Upload() {
 
     // Maneja la respuesta
     if (response.ok) {
-      // Haz algo con la respuesta si es exitosa, por ejemplo, mostrar un mensaje de éxito
+      console.log("Funciona")
     } else {
-      // Maneja el caso de error
+      console.log("No funciona")
     }
 
     setSubmitting(false);
@@ -44,52 +44,52 @@ export default function Upload() {
       <Header />
 
       <div className="max-w-7xl mx-auto border-2 border-white m-9 rounded-md">
-        <section class="max-w-7xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800">
-          <h1 class="text-xl font-bold text-white capitalize dark:text-white">
+        <section className="max-w-7xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800">
+          <h1 className="text-xl font-bold text-white capitalize dark:text-white">
             Upload Content
           </h1>
           <form onSubmit={handleSubmit}>
-            <div class="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
               <div>
-                <label class="text-white dark:text-gray-200" for="username">
+                <label className="text-white dark:text-gray-200" htmlFor="username">
                   Username
                 </label>
                 <input
                   name="username"
                   id="username"
                   type="text"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
 
               <div>
-                <label class="text-white dark:text-gray-200" for="emailAddress">
+                <label className="text-white dark:text-gray-200" htmlFor="emailAddress">
                   Email Address
                 </label>
                 <input
                   name="emailAddress"
                   id="emailAddress"
                   type="email"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
 
               <div>
-                <label class="text-white dark:text-gray-200" for="Title">
+                <label className="text-white dark:text-gray-200" htmlFor="Title">
                   Title
                 </label>
                 <input
                   name="title"
                   id="Title"
                   type="Title"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
 
               <div>
                 <label
-                  class="text-white dark:text-gray-200"
-                  for="titleConfirmation"
+                  className="text-white dark:text-gray-200"
+                  htmlFor="titleConfirmation"
                 >
                   Link Imagen
                 </label>
@@ -97,13 +97,13 @@ export default function Upload() {
                   name="linkImage"
                   id="titleConfirmation"
                   type="Title"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
               <div>
                 <label
-                  class="text-white dark:text-gray-200"
-                  for="titleConfirmation"
+                  className="text-white dark:text-gray-200"
+                  htmlFor="titleConfirmation"
                 >
                   Realease Date
                 </label>
@@ -111,13 +111,13 @@ export default function Upload() {
                   name="date"
                   id="date"
                   type="date"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 />
               </div>
               <div>
                 <label
-                  class="text-white dark:text-gray-200"
-                  for="titleConfirmation"
+                  className="text-white dark:text-gray-200"
+                  htmlFor="titleConfirmation"
                 >
                   Details
                 </label>
@@ -125,17 +125,17 @@ export default function Upload() {
                   name="details"
                   id="textarea"
                   type="textarea"
-                  class="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
                 ></textarea>
               </div>
               <div className="col-span-2">
-                <label class="block text-sm font-medium text-white">
+                <label className="block text-sm font-medium text-white">
                   Content
                 </label>
-                <div class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                  <div class="space-y-1 text-center">
+                <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                  <div className="space-y-1 text-center">
                     <svg
-                      class="mx-auto h-12 w-12 text-white"
+                      className="mx-auto h-12 w-12 text-white"
                       stroke="currentColor"
                       fill="none"
                       viewBox="0 0 48 48"
@@ -148,33 +148,33 @@ export default function Upload() {
                         stroke-linejoin="round"
                       />
                     </svg>
-                    <div class="flex text-sm text-gray-600">
+                    <div className="flex text-sm text-gray-600">
                       <label
-                        for="file-upload"
-                        class="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                        htmlFor="file-upload"
+                        className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
                       >
-                        <span class="">Upload a file</span>
+                        <span className="">Upload a file</span>
                         <input
                           id="file-upload"
                           name="file-upload"
                           type="file"
-                          class="sr-only"
+                          className="sr-only"
                           accept="video/*"
                         />
                       </label>
-                      <p class="pl-1 text-white">or drag and drop</p>
+                      <p className="pl-1 text-white">or drag and drop</p>
                     </div>
-                    <p class="text-xs text-white">MP4, WAV</p>
+                    <p className="text-xs text-white">MP4, WAV</p>
                   </div>
                 </div>
               </div>
             </div>
 
-            <div class="flex justify-end mt-6">
+            <div className="flex justify-end mt-6">
               <button
                 type="submit"
                 disabled={submitting}
-                class="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600"
+                className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600"
               >
                 {submitting ? "Sending..." : "Send"}
               </button>
